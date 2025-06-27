d40c85087049c3f94c1b841eee4ba9000f04b3fb
Deepshikha2608 <deepshikhaverma511@gmail.com>
2025-06-27 16:02:32 +0530

    Update Synchronizer_test.v

--- Synchronizer_test.v
+++ Synchronizer_test.v
@@ -4,9 +4,9 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   17:53:54 06/13/2019
+// Create Date:   17:53:54 26/12/2024
 // Design Name:   Synchronizer
-// Module Name:   C:/Users/Aruna/Documents/ISE/TrafficLightController/Synchronizer_test.v
+// Module Name:   C:/Users/deeps/Documents/ISE/TrafficLightController/Synchronizer_test.v
 // Project Name:  TrafficLightController
 // Target Device:  
 // Tool versions:  
